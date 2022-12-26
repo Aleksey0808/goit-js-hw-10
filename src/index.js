33ea3bdf269aaf8be904ad83c103cc421e17425e
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2022-12-26 22:14:51 +0200

    261222

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
 
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+};
+const inputValue = refs.inputEl.textContent.value;
+
 const DEBOUNCE_DELAY = 300;
+
+function fetchCountries(name) {
+  return fetch('https://restcountries.com/v3.1/name/`${name}`').then(response =>
+    response.json()
+  );
+}
+
+fetchCountries(inputValue)
+  .then(country => console.log(country))
+  .catch(error => error);
