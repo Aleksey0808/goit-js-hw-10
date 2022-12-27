94f1dc0e73329e1b5fa12bad0c44aa6d57f1eef7
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2022-12-27 21:57:41 +0200

    2712222

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import { fetchCountries  } from './js/fetchCountries';
-import { Notify } from 'notiflix';
+import fetchCountries from './js/fetchCountries';
+import Notify from 'notiflix';
 
 const refs = {
   inputEl: document.querySelector('#search-box'),
@@ -12,30 +12,25 @@ const refs = {
 const DEBOUNCE_DELAY = 300;
 let searchCountry = '';
 
-refs.inputEl.addEventListener("input", debounce(textInput, DEBOUNCE_DELAY))
-
+refs.inputEl.addEventListener('input', debounce(textInput, DEBOUNCE_DELAY));
 
 function textInput(e) {
   e.preventDefault();
   let searchCountry = refs.inputEl.value;
-if (searchCountry.trim() === '') {
-  refs.countryInfo.innerHTML = '';
-  refs.countryList.innerHTML = '';
-  return;
-}
+  if (searchCountry.trim() === '') {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+    return;
+  }
 }
 
 fetchCountries(searchCountry.trim())
-.then(country => {
-  console.log(country);
-  if (country.length > 10) {
-    Notify.info("Too many matches found. Please enter a more specific name.");
-    refs.countryInfo.innerHTML = '';
-    refs.countryList.innerHTML = '';
-  }
-})
-.catch(error => error);
-
-
-  
-
+  .then(country => {
+    console.log(country);
+    if (country.length > 10) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+      refs.countryInfo.innerHTML = '';
+      refs.countryList.innerHTML = '';
+    }
+  })
+  .catch(error => error);
